10c91fdcc0fd328085f85b9be9593d77119a23b1
Abdurahman <abdorehman@gmail.com>
2023-10-15 00:37:15 -0600

    Add the Favourite Icon Component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,22 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  // Define a state called 'isFavorited' and a function to update it named 'setIsFavorited'
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleFavClick = useCallback(() => {
+    setIsFavorited(prevState => !prevState);// Toggle the state using the 'setIsFavorited' and the previous state value
+    console.log(`Image is now ${isFavorited ? 'unfavorited' : 'favorited'}`);
+  }, [isFavorited]);
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleFavClick}>
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
+        <FavIcon
+          selected={isFavorited}
+          onClick={handleFavClick}
+        />
       </div>
     </div>
   );
