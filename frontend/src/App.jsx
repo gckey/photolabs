e5a18fc6be8b1360b2cc4dc7a1c56bbc8f1f14c7
Abdurahman <abdorehman@gmail.com>
2023-10-15 00:34:27 -0600

    Display 3 photos using array for the PhotoListItems component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,39 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoFavButton from './components/PhotoFavButton';
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 
 // Note: Rendering a single component to build components in isolation
+
 const App = () => {
+  //create an array with 3 elements and populate the array
+  const photos = new Array(3).fill(sampleDataForPhotoListItem);
   return (
     <div className="App">
-      <PhotoListItem/>
+      {photos.map((data, index) => ( //map to iterate each elts in photos
+        <div key={index}>
+          <PhotoFavButton />
+          <PhotoListItem
+            id={data.id}
+            city={data.location.city}
+            country={data.location.country}
+            imageSource={data.imageSource}
+            username={data.username}
+            profile={data.profile}
+          />
+        </div>
+      ))}
     </div>
   );
 };
