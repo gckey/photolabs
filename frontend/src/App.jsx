fccb72a02badd7ca5cb9ae3f44deaa849208d01f
Abdurahman <abdorehman@gmail.com>
2023-10-16 13:52:31 -0600

    Remove sample data and PhotoFavButton function call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,15 @@
 import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoFavButton from './components/PhotoFavButton';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+import TopicList from 'components/TopicList';
+import PhotoList from 'components/PhotoList';
 
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
-  //create an array with 3 elements and populate the array
-  const photos = new Array(3).fill(sampleDataForPhotoListItem);
   return (
     <div className="App">
-      {photos.map((data, index) => ( //map to iterate each elts in photos
-        <div key={index}>
-          <PhotoFavButton />
-          <PhotoListItem
-            id={data.id}
-            city={data.location.city}
-            country={data.location.country}
-            imageSource={data.imageSource}
-            username={data.username}
-            profile={data.profile}
-          />
-        </div>
-      ))}
+      <TopicList />
+      <PhotoList />
     </div>
   );
 };
