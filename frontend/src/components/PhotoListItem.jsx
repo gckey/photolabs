faf8d4243f9238075f0dbf8c4818ed7579ce61c9
Abdurahman <abdorehman@gmail.com>
2023-10-16 13:47:43 -0600

    Refracotor names in className, add div element

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
   return (
-    <div className="PhotoListItem">
-      <img className="photoURL" src={sampleDataForPhotoListItem.imageSource} />
-      <img className="profile_image" src={sampleDataForPhotoListItem.profile} />
-      <h3 className="photographer_username">{sampleDataForPhotoListItem.username}</h3>
-      <h3 className="photo_location">{sampleDataForPhotoListItem.location.city} {sampleDataForPhotoListItem.location.country}</h3>
+    <div className="photo-list__item">
+      <PhotoFavButton />
+      <img className="photo-list__image" src={props.imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.profile} />
+        <div className="photo-list__user-info">
+          <span>{props.username}</span>
+          <br />
+          <span className="photo-list__user-location">{` ${props.city}, ${props.country}`}</span>
+        </div>
+      </div>
     </div>
   );
 };
