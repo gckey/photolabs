2049395313277d30cc889392231a53ea30cd46ae
Abdurahman <abdorehman@gmail.com>
2023-10-16 23:13:16 -0600

    create the home page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.scss';
-import TopicList from 'components/TopicList';
-import PhotoList from 'components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
-import FavBadge from 'components/FavBadge';
+import HomeRoute from 'components/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
   return (
-    <>
-      <div className="App">
-        <div className="top-nav-bar">
-          <TopNavigation />
-          <TopicList />
-          <FavBadge />
-        </div>
-      </div>
-      <PhotoList />
-    </>
+    <div className="App">
+      <HomeRoute />
+    </div>
   );
 };
 
