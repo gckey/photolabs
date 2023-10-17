ffce431484a87c31716a47f3f9413d72a53b421c
Abdurahman <abdorehman@gmail.com>
2023-10-16 21:54:13 -0600

    render components within App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import './App.scss';
 import TopicList from 'components/TopicList';
 import PhotoList from 'components/PhotoList';
+import TopNavigation from 'components/TopNavigationBar';
+import FavBadge from 'components/FavBadge';
 
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
   return (
-    <div className="App">
-      <TopicList />
+    <>
+      <div className="App">
+        <div className="top-nav-bar">
+          <TopNavigation />
+          <TopicList />
+          <FavBadge />
+        </div>
+      </div>
       <PhotoList />
-    </div>
+    </>
   );
 };
 
