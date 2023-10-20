4085bc958bdc1306248d8813fc6d1c018c357a04
Abdurahman <abdorehman@gmail.com>
2023-10-19 22:57:04 -0600

    Refractor code by passing photo details to output to the console

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,10 +4,16 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
+  const { photo, closeToggleModal } = props;
+
   const handleCloseModalClick = () => {
+    closeToggleModal();
     props.closeToggleModal();
     console.log("Modal close btn clicked");
   }
+
+  console.log("Selected Photo:", photo);//Get selected all the details of the photo
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
