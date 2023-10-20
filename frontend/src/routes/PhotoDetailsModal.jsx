4471f332a8272eabccbeb066f33c1d596aa369a5
Abdurahman <abdorehman@gmail.com>
2023-10-19 20:00:45 -0600

    Add handleCloseModalClick function

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+  const handleCloseModalClick = () => {
+    props.closeToggleModal();
+    console.log("Modal close btn clicked");
+  }
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+        <img src={closeSymbol} alt="close symbol" onClick={handleCloseModalClick}/>
       </button>
     </div>
   )
