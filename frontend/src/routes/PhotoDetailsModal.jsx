69c0ea6fa586f83cb53f2b0eb2877be518ed658e
Abdurahman <abdorehman@gmail.com>
2023-10-20 15:49:44 -0600

    Add functionality to view a larger version of the photo and similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import photos from '../mocks/photos';
+import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = (props) => {
   const { photo, closeToggleModal } = props;
@@ -17,8 +19,33 @@ const PhotoDetailsModal = (props) => {
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={handleCloseModalClick}/>
+        <img src={closeSymbol} alt="close symbol"
+          onClick={handleCloseModalClick} 
+        />
       </button>
+      <div className="photo-details-modal__images">
+        <img className="photo-details-modal__image"
+          src={photo.urls.full}
+        />
+        <section className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile"
+            src={photo.user.profile} />
+          <div className="photo-details-modal__photographer-info">
+            <span>{photo.user.name}</span>
+            <br />
+            <span className="photo-details-modal__photographer-location">
+              {` ${photo.location.city}, ${photo.location.country}`}
+            </span>
+          </div>
+        </section>
+        <div className="photo-details-modal__header"><h3>Similar Photos</h3></div>
+      </div>
+
+      <PhotoList
+        favoritedPhotos={[]} /*passing a prop favoritedPhotos to the PhotoList*/
+        toggleFavorite={() => { }} /*/passing props to PhotoList*/
+        toggleModal={() => { }} /*/passing props to PhotoList*/
+      />
     </div>
   )
 };
