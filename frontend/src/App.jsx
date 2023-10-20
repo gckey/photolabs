e275ce3323086640465df887f63d491e01f7d0b5
Abdurahman <abdorehman@gmail.com>
2023-10-19 19:05:08 -0600

    Add functional components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from 'components/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
+  //state that manages modal visibility
+  const [modal, setModal] = useState(false);//false we need to see only when image is clicked i.e true
+
+  const toggleModal = () => {
+    setModal(!modal);
+  }
+
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute 
+        toggleModal={toggleModal}
+      />
+      {modal && <PhotoDetailsModal modal={modal} />} {/*short circuit opp, render the modal conditionally, if it's true return modal*/}
     </div>
   );
 };
