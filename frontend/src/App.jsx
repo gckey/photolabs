be3e4461f5ad9e5874f85fadc9987285b9bb14ed
Abdurahman <abdorehman@gmail.com>
2023-10-25 14:32:50 -0600

    pass the props down the chain

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.scss';
-import HomeRoute from 'components/HomeRoute';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
-import useApplicationData from 'hooks/useApplicationData';
+import React from "react";
+import "./App.scss";
+import HomeRoute from "components/HomeRoute";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import useApplicationData from "hooks/useApplicationData";
 
 // Note: Rendering a single component to build components in isolation
 
@@ -20,16 +20,20 @@ const App = () => {
         toggleModal={toggleModal} //func passed down to HomeRoute
         closeToggleModal={closeToggleModal} //func passed down to HomeRoute
         favoritedPhotos={state.favoritedPhotos}//array of favorited photo IDs passed down to HomeRoute.jsx
+        photoData={state.photoData}
+        topics={state.topicsData}
         toggleFavorite={toggleFavorite}//func passed down to HomeRoute
       />
       {/* {modal && <PhotoDetailsModal modal={modal} />} short circuit opp, render the modal conditionally, if it's true return modal */}
 
       {/*render PhotoDetailsModal with various props */}
       {state.modal && <PhotoDetailsModal
+        state={state}
         photo={state.selectedPhoto}
         closeToggleModal={closeToggleModal}
         favoritedPhotos={state.favoritedPhotos}
         toggleFavorite={toggleFavorite}
+        toggleModal={toggleModal}
       />}{/*render the modal conditionally */}
     </div>
   );
