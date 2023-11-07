783cfeb3c14fed3b6c2806efccd481456f629725
Abdurahman Mohammed <abdorehman@gmail.com>
2023-11-06 20:41:39 -0800

    Add handleTopicClick func

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 
 import "../styles/TopicListItem.scss";
-// import topics from '../mocks/topics';
-import { TopicList } from 'components/TopicList';
-
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  title: "Nature",
-};
 
 const TopicListItem = (props) => {
+
+  const handleTopicClick = () => {
+    props.fetchPhotosByTopic(props.id);
+  }
+
   return (
-    <div className="topic-list__item top-nav-bar__topic-list">
+    <div className="topic-list__item top-nav-bar__topic-list" onClick={handleTopicClick}>
       {/* Insert React */}
       <span className="topic-list__item">{props.title}</span>
       <i className="&:hover"></i>
